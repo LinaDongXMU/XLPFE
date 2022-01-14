c381ac7a3a84c5209f4642ff84a3a34e0f91d095
LinaDongXMU <90234051+LinaDongXMU@users.noreply.github.com>
2022-01-14 16:58:26 +0800

    Add files via upload

---
+++ examples/for_feature/combine_feature.py
@@ -0,0 +1,45 @@
+import csv
+
+xscore=open('./xscore.csv')
+xscoreReader=csv.reader(xscore)
+xscoreData=list(xscoreReader)
+ligand=open('./ligand.csv')
+ligandReader=csv.reader(ligand)
+ligandData=list(ligandReader)
+pocket=open('./pocket.csv')
+pocketReader=csv.reader(pocket)
+pocketData=list(pocketReader)
+fullprotein=open('./fullprotein.csv')
+fullproteinReader=csv.reader(fullprotein)
+fullproteinData=list(fullproteinReader)
+
+
+add=open('./test.csv','w')
+addWriter=csv.writer(add)
+
+d=['Name','VDW','HB','HP','HM','HS','RT','x-score','q','C','N','O','H','F','P','S','Cl','Br','I','1','2','3','am','ar','his','arg','lys','ile','phe','leu','trp','ala','met','pro','val','cys','asn','gly','ser','gln','tyr','thr','asp','glu','hoh','phis','parg','plys','pile','pphe','pleu','ptrp','pala','pmet','ppro','pval','pcys','pasn','pgly','pser','pgln','ptyr','pthr','pasp','pglu']
+addWriter.writerow(d)
+
+for i in xscoreData:
+    a=i[0]
+    for j in ligandData:
+        b=j[0]
+        if a==b:
+            c=i
+            c=c+j[1:]
+            for k in pocketData:
+                x=k[0]
+                if x==b:
+                    c=c+k[1:]
+                    for l in fullproteinData:
+                        y=l[0]
+                        if y==x:
+                            c=c+l[1:]
+                            addWriter.writerow(c)
+
+
+xscore.close()
+ligand.close()
+pocket.close()
+fullprotein.close()
+add.close()
